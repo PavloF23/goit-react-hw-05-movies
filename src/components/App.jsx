bad8a4e751e620acdd4d71687390d198e98accc8
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-01-27 21:45:13 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "path/to/pages/Home";
+import About from "path/to/pages/About";
+import Products from "path/to/pages/Products";
+import NotFound from "path/to/pages/NotFound";
+import ProductDetails from "path/to/pages/ProductDetails";
+
 export const App = () => {
   return (
-    <div
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+          <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -12,5 +31,7 @@ export const App = () => {
     >
       React homework template
     </div>
-  );
-};
+        </Routes>
+      </div>
+    );
+  };
