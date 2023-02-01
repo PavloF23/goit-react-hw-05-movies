60a5d263021a9b2f98bb7e873b46a3a01f2ac589
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-02-01 14:00:15 +0200

    up

---
+++ src/components/Cast.jsx
@@ -0,0 +1,52 @@
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useMount } from 'react-use';
+import { getAxiosActors } from "../Api/getAxios";
+import SharedLoader from './SharedLoader/SharedLoader';
+
+export const Cast = () => {
+  const { movieId } = useParams();
+  const [filmActors, setFilmActors] = useState([]);
+  const [isLoading, setIsLoadings] = useState(false);
+  const [error, setError] = useState('');
+
+  useMount(() => {
+    async function getFilm() {
+      try {
+        const searchActors = await getActors(movieId)
+        
+        if (searchActors.data.cast.length !== 0) {
+          setFilmActors(searchActors.data.cast)
+          setIsLoadings(false)
+          return 
+         }
+        setIsLoadings(false)
+        setFilmActors([])
+        throw new Error("Sory, no result!");
+      } catch (error) {
+        setIsLoadings(false)
+        setError(error)
+        setFilmActors([])
+        console.log(error);
+      }
+    }
+
+    setIsLoadings(true)
+    getFilm()      
+  })
+  return (
+    <section>
+      {isLoading && <Loader />}
+      {error && <h2>{error.message}</h2>}
+      {filmActors.length !== 0 && <ul>
+        {filmActors.map((actor) => (<li key={actor.id}>
+          {actor.profile_path && <img width={100} src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt="" />}
+          {!actor.profile_path && <img width={100} src={"https://www.meme-arsenal.com/memes/fefac21eda463aa9a307c7cfdbea1bee.jpg"} alt="" />}
+          <p>{actor.name}</p>
+          <p>Character: {actor.character}</p>
+        </li>
+        ))}
+      </ul>}
+    </section>
+  );
+};
