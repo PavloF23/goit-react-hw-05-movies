20142cda6a4b0818c38edc8928e64b97299dcc52
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-02-01 14:17:17 +0200

    up

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 // import { TbHome, TbMovie } from 'react-icons/tb';
 import { Appbar, Container, Navigation, NavItem } from './SharedLayout.styled';
-import { Suspense } from 'react';
 
 export const SharedLayout = () => {
   return (
