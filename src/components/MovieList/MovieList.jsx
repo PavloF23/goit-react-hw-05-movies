5defdca901a7d19807fb875881a868a35be53493
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-02-02 11:14:48 +0200

    up

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,24 +3,24 @@ import { useLocation } from 'react-router-dom';
 import { List, ListItem, Item, Poster, MovieInfo } from './MovieList.styled';
 import defaultPicture from '../../images/default-movie.jpg';
 
-export const MovieList = ({ films }) => {
+export const MovieList = ({ items }) => {
   const location = useLocation();
 
   return (
     <List>
-      {films.map((film) => (
-        <ListItem key={film.id}>
-          <Item to={`/movies/${film.id}`} state={{ from: location }}>
+      {films.map(({ id, poster_path, title }) => (
+        <ListItem key={id}>
+          <Item to={`/movies/${id}`} state={{ from: location }}>
             <Poster
               src={
-                item.poster_path
-                  ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
+                poster_path
+                  ? `https://image.tmdb.org/t/p/w500${poster_path}`
                   : defaultPicture
               }
-              alt={film.title}
+              alt={title}
             />
             <MovieInfo>
-              <p>{film.title}</p>
+              <p>{title}</p>
             </MovieInfo>
           </Item>
         </ListItem>
