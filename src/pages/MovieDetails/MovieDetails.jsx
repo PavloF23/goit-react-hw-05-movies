15a9187abd38f01ecec924b1352bd4c0f7d300c6
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-02-04 12:22:23 +0200

    up

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,8 @@
 import { useParams, useLocation, Outlet } from "react-router-dom";
 import { useState, Suspense } from "react";
 // import { useRef } from "react";
-import { useMount } from 'react-use';
+import { useEffect } from "react";
+// import { useMount } from 'react-use';
 import { AiFillCaretLeft } from "react-icons/ai";
 import { Main, ButtonBack, FilmInfo, Title, Description, List, LinkBtn } from './MovieDetails.styled'
 import { getMoviesID } from "../../components/servisApi";
@@ -13,10 +14,12 @@ const MovieInfo = () => {
   const [filmId, setFilmId] = useState('');
   const [genre, setGenre] = useState([]);
   const [date, setDate] = useState('');
-  const [loc, setLoc] = useState('');
-  // const locRef = useRef(location.stete?.from ?? '/movies');
+  // const [loc, setLoc] = useState('');
+  // const locRef = useRef(location.stete?.from ?? '/');
+  const locRef = location.stete?.from ?? '/';
    
-  useMount(() => {
+  // useMount(() => {
+    useEffect(() => {
     async function getFilm() {
       try {
         const filmInfo = await getMoviesID(movieId)
@@ -36,14 +39,17 @@ const MovieInfo = () => {
       }
     }
     
-    setLoc(location.state?.from ?? "/movies")
+    // setLoc(location.state?.from ?? "/")
     getFilm()    
-    })
+    }
+    , [movieId]
+    )
     
   return (
     <Main>
       {/* <ButtonBack to={locRef.current}> */}
-      <ButtonBack to={loc}>
+      {/* <ButtonBack to={loc}> */}
+      <ButtonBack to={locRef}>
         <AiFillCaretLeft />
         <p>Come Back</p>
       </ButtonBack>
@@ -67,7 +73,7 @@ const MovieInfo = () => {
         <p>Additional information</p>
         <List>
           <li>
-            <LinkBtn to='cast' filmid={movieId}>Cast</LinkBtn>
+            <LinkBtn to='cast' state={{ from: locRef }} filmid={movieId}>Cast</LinkBtn>
           </li>
           <li>
             <LinkBtn to="reviews" filmid={movieId}>Reviews</LinkBtn>
